537e9f37135b2e5b07ae87e228515ece85298d54
devalexandre <alexandre@indev.net.br>
2020-03-09 16:59:43 -0300

    Preact + NustJS + Styled-Components

---
+++ components/Title/index.js
@@ -0,0 +1,7 @@
+import { h } from 'preact'
+import * as S from './styled'
+
+
+const Title = () => <S.Title>Preact + NustJS + Styled-Components</S.Title>
+
+export default Title
---
+++ pages/index.js
@@ -0,0 +1,4 @@
+import { h }from 'preact'
+ import Title from '../components/Title'
+
+export default () => <Title>My page</Title>
